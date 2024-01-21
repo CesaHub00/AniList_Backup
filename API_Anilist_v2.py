37d6a2e492a18349b1ec9cbb986d396f6642ad88
Cesare Nava <cesarenava247@gmail.com>
2024-01-21 15:39:00 +0100

    Add files via upload

---
+++ API_Anilist_v2.py
@@ -0,0 +1,111 @@
+##########################################
+# With requests method, taking all the entry from a dinamyc website and copiyng them on a note on the desktop.
+# Solution: Load all the entry before.
+##########################################
+
+from datetime import datetime
+import json
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+# scroll page function
+def scrolldown(Url):
+    
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    SCROLL_PAUSE_TIME = 5.0
+
+    # initiating web driver
+    driver.get(Url)
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+
+        # scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+    
+    # getting the same page (in link) after full load 
+    get_url = driver.page_source
+
+    # ready for parsing
+    results = BeautifulSoup(get_url, features="html.parser")
+
+    return results
+
+# Url
+OG_Url = 'https://anilist.co/user/Cesa00/animelist'
+
+# driver intsall
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+options = webdriver.ChromeOptions()
+
+# creating lists
+Watching_list = []
+Completed_list = []
+Planning_list = []
+Rewatching_list = []
+Dropped_list = []
+
+# scroll results (scroll page function)
+results = scrolldown(OG_Url)
+
+# parsing
+tot_elements = results.find_all("div", class_="list-wrap")
+for tot_element in tot_elements:
+
+    # section
+    status_element = tot_element.find("h3", class_="section-name")
+    status_element = status_element.text.strip().upper()
+    
+    # title
+    links = tot_element.find_all("a")
+    for link in links:
+        link = link.text.strip().lower().capitalize()
+
+        if status_element == "WATCHING":
+            Watching_list.append(link)
+        elif status_element == "REWATCHING":
+            Rewatching_list.append(link)
+        elif status_element == "COMPLETED":
+            Completed_list.append(link)
+        elif status_element == "PLANNING":
+            Planning_list.append(link)
+        elif status_element == "DROPPED":
+            Dropped_list.append(link)
+
+##
+## SAVING ON FILE
+##
+# get current date and time
+current_date = datetime.now().strftime("%Y-%m-%d")
+print("Current date: ", current_date)
+ 
+# convert datetime obj to string
+str_current_datetime = str(current_date)
+
+# dictionary with lists and total length
+lines = {
+        "TOTAL": len(Watching_list) + len(Rewatching_list) + len(Dropped_list) + len(Completed_list) + len(Planning_list),
+        "WATCHING": [len(Watching_list), Watching_list],
+        "REWATCHING": [len(Rewatching_list), Rewatching_list],
+        "DROPPED": [len(Dropped_list), Dropped_list],
+        "COMPLETED": [len(Completed_list), Completed_list],
+        "PLANNING": [len(Planning_list), Planning_list]
+        }
+
+# file
+with open('Anilist '+ str_current_datetime +'.txt', 'w') as list:
+    json.dump(lines, list, indent=3)
+print("Done writing dict into .txt file")
+
